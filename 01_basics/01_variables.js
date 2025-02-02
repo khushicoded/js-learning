f9ea10a63065fc340bc9e271839858d604cb3d2a
Khushi Gupta <khushi.coded@gmail.com>
2025-02-02 20:43:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "khushi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121212121"
+accountCity = "Bangbang"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
